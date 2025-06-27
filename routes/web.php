d82e3d44f7dbf69fb2c06ec0a1726d382d5317dc
Jied Joshua <118013854+jiedjoshua@users.noreply.github.com>
2025-06-27 22:07:35 +0800

    terms

--- routes/web.php
+++ routes/web.php
@@ -40,14 +40,6 @@ Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
 
-Route::get('/terms-and-conditions', function () {
-    return view('terms-and-conditions');
-})->name('terms-and-conditions');
-
-Route::get('/privacy-policy', function () {
-    return view('privacy-policy');
-})->name('privacy-policy');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
