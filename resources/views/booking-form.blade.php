283aa6f1b00943bd3e8e0cf4ffe77be201ec2a74
Jied Joshua <118013854+jiedjoshua@users.noreply.github.com>
2025-06-27 20:59:31 +0800

    css update path

--- resources/views/booking-form.blade.php
+++ resources/views/booking-form.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Event Booking Form</title>
-    <link rel="stylesheet" href="{{ asset('css/booking-form.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/booking-form.css') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
