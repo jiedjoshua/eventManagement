a0e0ddee7d7e7568234af39df87ec9e86e860cac
Jied Joshua <118013854+jiedjoshua@users.noreply.github.com>
2025-06-30 01:34:15 +0800

    qr scanner side bar

--- resources/views/manager/manage-events/view/qrscanner.blade.php
+++ resources/views/manager/manage-events/view/qrscanner.blade.php
@@ -37,7 +37,7 @@
             <div>
                 <p class="mt-4 font-semibold text-gray-900">Check-in Controls</p>
                 <a href="{{ route('events.qrScanner', ['event' => $event->id]) }}" class="block pl-4 py-2 rounded bg-indigo-200 font-semibold text-indigo-800">QR Scanner</a>
-                <a href="{{ route('events.checkedIn', ['event' => $event->id]) }}" class="block pl-4 py-2 hover:bg-indigo-100 rounded">Manual Check-in</a>
+                <a href="{{ route('events.manualCheckin', ['event' => $event->id]) }}" class="block pl-4 py-2 hover:bg-indigo-100 rounded">Manual Check-in</a>
                 <a href="{{ route('events.checkedIn', ['event' => $event->id]) }}" class="block pl-4 py-2 hover:bg-indigo-100 rounded">Checked in Guests</a>
             </div>
 
